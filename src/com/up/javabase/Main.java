45d54aa9fb0e1e286134690bc0b860bbeb29cd62
fanfanfly9527 <fanlingyunfly@126.com>
2018-04-06 23:35:50 +0800

    Initial commit

---
+++ src/com/up/javabase/Main.java
@@ -0,0 +1,26 @@
+package com.up.javabase;
+
+public class Main {
+
+    public static void main(String[] args) {
+       /* System.out.println("Hello World!");
+        String[] greeting = new String[3];
+        greeting[0] = "welconme to Core Java";
+        greeting[1] = "by Cay Horsmann";
+        greeting[2] = "and Gary Cornell";
+
+        for(String g : greeting) {
+            System.out.println(g);
+        }
+        */
+        for (int i:new int[]{1, 2, 3}) {
+            if(i!=2)
+                System.out.println("*********************************************");
+            else
+                System.out.println("***************  Java  程序设计  **************");
+
+
+        }
+    }
+}
+
